afa139f00c1c79a4ae3de532f4babc63698c7939
unknown <matthewmccollum17@gmail.com>
2019-11-03 17:38:07 -0500

    Added individual items to preview

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./pages/homepage/Homepage.scss";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />{" "}
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
