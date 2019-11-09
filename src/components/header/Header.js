3f8c9b540d30b522962723bf5f4f7744575e91cf
unknown <matthewmccollum17@gmail.com>
2019-11-08 20:42:18 -0500

    Added redux functionality to cart icon and the dropdown menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './Header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -21,12 +23,16 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {!hidden && <CartDropdown />}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+
 });
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/actions.js
@@ -0,0 +1,5 @@
+import * as actionTypes from './actionTypes';
+
+export const toggleCartHidden = () => ({
+  type: actionTypes.TOGGLE_CART_HIDDEN
+});
