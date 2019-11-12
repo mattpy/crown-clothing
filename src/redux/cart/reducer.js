4fe8a628eaafd04bfa2ee502ed73eafd1a7a9f80
unknown <matthewmccollum17@gmail.com>
2019-11-11 21:23:19 -0500

    Added buttons to remove items from checkout

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,5 +1,5 @@
-import * as actionTypes from './actionTypes';
-import { addItemToCart } from './cartUtils';
+import * as actionTypes from "./actionTypes";
+import { addItemToCart, removeItemFromCart } from "./cartUtils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -8,17 +8,30 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case actionTypes.TOGGLE_CART_HIDDEN: 
+    case actionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden
-      }
+      };
 
     case actionTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+    case actionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+      };
+
+    case actionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       }
+
     default:
       return state;
   }
